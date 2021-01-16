a76308c05f6fe1a1b501b2c523f0e4a285207f76
Jack Cai <caixunshiren@gmail.com>
2021-01-16 14:55:13 -0500

    1

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -28,13 +28,13 @@ void GameBoard::CreatePlayer()
 	m_player = new GameEngine::Entity();
 	GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
 
-	m_player->SetPos(sf::Vector2f(50.0f, 50.0f));
+	m_player->SetPos(sf::Vector2f(200.0f, 300.0f));
 	m_player->SetSize(sf::Vector2f(50.0f, 50.0f));
 
 	//Render
 	GameEngine::SpriteRenderComponent* render = m_player->AddComponent<GameEngine::SpriteRenderComponent>(); //<-- Use the SpriteRenderComponent
 	
-	render->SetFillColor(sf::Color::Transparent);
+	render->SetFillColor(sf::Color::Red);
 	render->SetTexture(GameEngine::eTexture::Player);  // <-- Assign the texture to this entity
 	
 	//Movement
